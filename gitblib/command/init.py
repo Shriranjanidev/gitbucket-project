4f0fba79087fdb1377f1dfdb856f2a4dc55c1c09
Shri Jammy <shrijammy@Shris-MacBook-Pro.local>
2020-10-12 13:34:12 +0530

    setup files

---
+++ gitblib/command/init.py
@@ -0,0 +1,9 @@
+from gitblib.command.command import Command 
+from gitblib.utility.createrepo import repo_create
+
+class Init(Command):
+    def execute(self, args):
+        print('TODO init')
+        repo_create(args.path)
+
+    
