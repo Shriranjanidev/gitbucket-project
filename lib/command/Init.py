572dd57f1be7554d4b0c6d820c8cd626199f8917
Shri Jammy <shrijammy@Shris-MacBook-Pro.local>
2020-10-12 12:53:02 +0530

    Initial commit

---
+++ lib/command/Init.py
@@ -0,0 +1,9 @@
+from .command import Command 
+from .utility.createrepo import repo_create
+
+class Init(Command):
+    def execute(self, args):
+        print('TODO init')
+        repo_create(args.path)
+
+    
